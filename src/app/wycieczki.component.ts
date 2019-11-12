3a29cec036b2f1b4edd2fc4cbf9940ce8b49528f
SG0302219 <szymon.sadowski@sabre.com>
2019-11-12 13:12:49 +0100

    - finding min/max element

--- src/app/wycieczki.component.ts
+++ src/app/wycieczki.component.ts
@@ -6,97 +6,111 @@ import { Component } from '@angular/core';
   styleUrls: ['./wycieczki.component.css']
 })
 export class WycieczkiComponent {
-  items = [
-    {
-      nazwa: "Dwutygodniowa wycieczka do Egiptu",
-      docelowyKrajWycieczki: "Egipt",
-      dataRozpoczecia: "2019-12-12",
-      dataZakonczenia: "2019-12-26",
-      cenaJednostkowa: "3000zł",
-      maxIloscMiejsc: 10,
-      opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
-      linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
-      ileZarezerwowano: 0,
-    },
-    {
-      nazwa: "Dwutygodniowa wycieczka do Rosji",
-      docelowyKrajWycieczki: "Rosja",
-      dataRozpoczecia: "2019-12-12",
-      dataZakonczenia: "2019-12-26",
-      cenaJednostkowa: "3000zł",
-      maxIloscMiejsc: 15,
-      opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
-      linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
-      ileZarezerwowano: 0,
-    },
-    {
-      nazwa: "Dwutygodniowa wycieczka do USA",
-      docelowyKrajWycieczki: "USA",
-      dataRozpoczecia: "2019-12-12",
-      dataZakonczenia: "2019-12-26",
-      cenaJednostkowa: "3000zł",
-      maxIloscMiejsc: 5,
-      opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
-      linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
-      ileZarezerwowano: 0,
-    },
-    {
-      nazwa: "Tygodniowa wycieczka do Egiptu",
-      docelowyKrajWycieczki: "Egipt",
-      dataRozpoczecia: "2019-12-12",
-      dataZakonczenia: "2019-12-19",
-      cenaJednostkowa: "3000zł",
-      maxIloscMiejsc: 7,
-      opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
-      linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
-      ileZarezerwowano: 0,
-    },
-    {
-      nazwa: "Tygodniowa wycieczka do Rosji",
-      docelowyKrajWycieczki: "Rosja",
-      dataRozpoczecia: "2019-12-12",
-      dataZakonczenia: "2019-12-19",
-      cenaJednostkowa: "3000zł",
-      maxIloscMiejsc: 9,
-      opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
-      linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
-      ileZarezerwowano: 0,
-    },
-    {
-      nazwa: "Tygodniowa wycieczka do USA",
-      docelowyKrajWycieczki: "USA",
-      dataRozpoczecia: "2019-12-12",
-      dataZakonczenia: "2019-12-19",
-      cenaJednostkowa: "3000zł",
-      maxIloscMiejsc: 4,
-      opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
-      linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
-      ileZarezerwowano: 0,
-    },
+  items;
+  maxElement;
+  minElement;
 
-    {
-      nazwa: "Dwudniowa wycieczka do Austrii",
-      docelowyKrajWycieczki: "Austria",
-      dataRozpoczecia: "2019-12-12",
-      dataZakonczenia: "2019-12-14",
-      cenaJednostkowa: "3000zł",
-      maxIloscMiejsc: 6,
-      opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
-      linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
-      ileZarezerwowano: 0,
-    },
-    {
-      nazwa: "Dwudniowa wycieczka do Czech",
-      docelowyKrajWycieczki: "Czechy",
-      dataRozpoczecia: "2019-12-12",
-      dataZakonczenia: "2019-12-14",
-      cenaJednostkowa: "3000zł",
-      maxIloscMiejsc: 11,
-      opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
-      linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
-      ileZarezerwowano: 0,
-    },
-  ];
+  constructor () {
+    this.items = [
+      {
+        nazwa: "Dwutygodniowa wycieczka do Egiptu",
+        docelowyKrajWycieczki: "Egipt",
+        dataRozpoczecia: "2019-12-12",
+        dataZakonczenia: "2019-12-26",
+        cenaJednostkowa: 1000,
+        maxIloscMiejsc: 10,
+        opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
+        linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
+        ileZarezerwowano: 0,
+      },
+      {
+        nazwa: "Dwutygodniowa wycieczka do Rosji",
+        docelowyKrajWycieczki: "Rosja",
+        dataRozpoczecia: "2019-12-12",
+        dataZakonczenia: "2019-12-26",
+        cenaJednostkowa: 2000,
+        maxIloscMiejsc: 15,
+        opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
+        linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
+        ileZarezerwowano: 0,
+      },
+      {
+        nazwa: "Dwutygodniowa wycieczka do USA",
+        docelowyKrajWycieczki: "USA",
+        dataRozpoczecia: "2019-12-12",
+        dataZakonczenia: "2019-12-26",
+        cenaJednostkowa: 3000,
+        maxIloscMiejsc: 5,
+        opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
+        linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
+        ileZarezerwowano: 0,
+      },
+      {
+        nazwa: "Tygodniowa wycieczka do Egiptu",
+        docelowyKrajWycieczki: "Egipt",
+        dataRozpoczecia: "2019-12-12",
+        dataZakonczenia: "2019-12-19",
+        cenaJednostkowa: 1100,
+        maxIloscMiejsc: 7,
+        opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
+        linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
+        ileZarezerwowano: 0,
+      },
+      {
+        nazwa: "Tygodniowa wycieczka do Rosji",
+        docelowyKrajWycieczki: "Rosja",
+        dataRozpoczecia: "2019-12-12",
+        dataZakonczenia: "2019-12-19",
+        cenaJednostkowa: 3000,
+        maxIloscMiejsc: 9,
+        opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
+        linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
+        ileZarezerwowano: 0,
+      },
+      {
+        nazwa: "Tygodniowa wycieczka do USA",
+        docelowyKrajWycieczki: "USA",
+        dataRozpoczecia: "2019-12-12",
+        dataZakonczenia: "2019-12-19",
+        cenaJednostkowa: 3000,
+        maxIloscMiejsc: 4,
+        opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
+        linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
+        ileZarezerwowano: 0,
+      },
+
+      {
+        nazwa: "Dwudniowa wycieczka do Austrii",
+        docelowyKrajWycieczki: "Austria",
+        dataRozpoczecia: "2019-12-12",
+        dataZakonczenia: "2019-12-14",
+        cenaJednostkowa: 3000,
+        maxIloscMiejsc: 6,
+        opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
+        linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
+        ileZarezerwowano: 0,
+      },
+      {
+        nazwa: "Dwudniowa wycieczka do Czech",
+        docelowyKrajWycieczki: "Czechy",
+        dataRozpoczecia: "2019-12-12",
+        dataZakonczenia: "2019-12-14",
+        cenaJednostkowa: 5000,
+        maxIloscMiejsc: 11,
+        opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel velit nulla. Nam malesuada efficitur maximus. Vestibulum eu maximus dolor. Cras commodo tortor aliquam lobortis pellentesque.",
+        linkDoZdj: "https://via.placeholder.com/100/09f/fff.png",
+        ileZarezerwowano: 0,
+      },
+    ];
+
+    this.maxElement = this.items.reduce((prev, current) => {
+      return (prev.cenaJednostkowa > current.cenaJednostkowa) ? prev : current
+    });
+
+    this.minElement = this.items.reduce((prev, current) => {
+      return (prev.cenaJednostkowa < current.cenaJednostkowa) ? prev : current
+    });
+  }
 
   onClickPlusButton(item) {
     if(item.ileZarezerwowano < item.maxIloscMiejsc) {
