e9f477d8512727a725bc85d927a0ebd7a03767e7
Szymon <szymonsadowski3@gmail.com>
2019-12-16 20:35:56 +0100

    - cypress tests progress

--- test/cypress/integration/sample_spec.js
+++ test/cypress/integration/sample_spec.js
@@ -31,8 +31,45 @@ class LoginScreen {
     return this.submitLogin();
   };
 };
-
 const loginScreen = new LoginScreen();
+
+class TripsScreen {
+  getTrips() {
+    return cy.get('.single-wycieczka');
+  };
+
+  getTripsMainSections() {
+    return cy.get('.single-wycieczka .main-section');
+  };
+
+  getFirstTrip() {
+    return this.getTrips()[0];
+  };
+
+  get() {
+    cy.visit(`${cfg.baseUrl}/${cfg.mainPageEndpoint}`);
+  };
+};
+const tripsScreen = new TripsScreen();
+
+class TripsDetails {
+  getAddTCartButton() {
+    return cy.get('.add-to-cart-button');
+  };
+};
+const tripsDetails = new TripsDetails();
+
+class Cart {
+  getOpenCartToggle() {
+    return cy.get('.open-cart-toggle');
+  };
+
+  getRows() {
+    return cy.get('tbody tr');
+  };
+};
+const cart = new Cart();
+
 
 describe('Wycieczki app', function() {
   it('Should open without crashing', function () {
@@ -48,5 +85,27 @@ describe('Wycieczki app', function() {
   it('Should allow user to log-in and redirect him to main page', function() {
     loginScreen.loginProcess();
     cy.location('pathname').should('eq', '/wycieczki');
+  });
+
+  it('Should display available trips to the logged-in user', function() {
+    loginScreen.loginProcess();
+    tripsScreen.get();
+    tripsScreen.getTrips().its('length').should('be.gt', 0);
+  });
+
+  it('Should allow user to view trip details', function() {
+    loginScreen.loginProcess();
+    tripsScreen.get();
+    tripsScreen.getTripsMainSections().first().click();
+    cy.location('pathname').should('contain', '/wycieczka');
+  });
+
+  it('Should allow user to add a trip to the cart', function() {
+    loginScreen.loginProcess();
+    tripsScreen.get();
+    tripsScreen.getTripsMainSections().first().click();
+    tripsDetails.getAddTCartButton().click();
+    cart.getOpenCartToggle().click();
+    cart.getRows().its('length').should('be.gt', 0);
   });
 });
