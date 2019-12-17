f0b956ad34614b2dd610f30afda9b8bf929a6d60
Szymon <szymonsadowski3@gmail.com>
2019-12-17 16:00:21 +0100

    - testing progress

--- test/cypress/integration/sample_spec.js
+++ test/cypress/integration/sample_spec.js
@@ -49,11 +49,16 @@ class TripsScreen {
   get() {
     cy.visit(`${cfg.baseUrl}/${cfg.mainPageEndpoint}`);
   };
+
+  route() {
+    cy.server();
+    cy.route(`/${mainPageEndpoint}`);
+  }
 };
 const tripsScreen = new TripsScreen();
 
 class TripsDetails {
-  getAddTCartButton() {
+  getAddToCartButton() {
     return cy.get('.add-to-cart-button');
   };
 };
@@ -64,48 +69,92 @@ class Cart {
     return cy.get('.open-cart-toggle');
   };
 
+  getGoToCartButton() {
+    return cy.get('.go-to-cart-button');
+  }
+
   getRows() {
     return cy.get('tbody tr');
   };
+
+  getCheckoutButton() {
+    return cy.get('.checkout-button').last();
+  }
+
+  getCheckoutConfirmButton() {
+    return cy.get('#cart > tfoot > tr > td:nth-child(4) > mwl-confirmation-popover-window > div > div.popover-content.popover-body > div > div:nth-child(2) > button');
+  }
+
+  getQuantityInput() {
+    return cy.get('.quantity-input').last();
+  }
 };
 const cart = new Cart();
 
+class AfterOrder {
+    getMessage() {
+      return cy.get('.thank-you-for-order-msg');
+    }
+
+    getEndpoint() {
+      return '/after-order';
+    }
+};
+const afterOrder = new AfterOrder();
+// Common functions
+
+function addFirstTripToCart() {
+  loginScreen.loginProcess();
+  tripsScreen.get();
+  tripsScreen.getTripsMainSections().first().click();
+  tripsDetails.getAddToCartButton().click();
+}
+
 
 describe('Wycieczki app', function() {
-  it('Should open without crashing', function () {
-    cy.visit(`${cfg.baseUrl}/${cfg.mainPageEndpoint}`);
-  });
-
-  it('Should have a title', function() {
-    cy.visit(`${cfg.baseUrl}/${cfg.mainPageEndpoint}`);
-
-    cy.title().should('eq', 'Wycieczki');
-  });
-
-  it('Should allow user to log-in and redirect him to main page', function() {
-    loginScreen.loginProcess();
-    cy.location('pathname').should('eq', '/wycieczki');
-  });
-
-  it('Should display available trips to the logged-in user', function() {
-    loginScreen.loginProcess();
-    tripsScreen.get();
-    tripsScreen.getTrips().its('length').should('be.gt', 0);
-  });
-
-  it('Should allow user to view trip details', function() {
-    loginScreen.loginProcess();
-    tripsScreen.get();
-    tripsScreen.getTripsMainSections().first().click();
-    cy.location('pathname').should('contain', '/wycieczka');
-  });
-
+  // it('Should open without crashing', function () {
+  //   cy.visit(`${cfg.baseUrl}/${cfg.mainPageEndpoint}`);
+  // });
+  //
+  // it('Should have a title', function() {
+  //   cy.visit(`${cfg.baseUrl}/${cfg.mainPageEndpoint}`);
+  //
+  //   cy.title().should('eq', 'Wycieczki');
+  // });
+  //
+  // it('Should allow user to log-in and redirect him to main page', function() {
+  //   loginScreen.loginProcess();
+  //   cy.location('pathname').should('eq', '/wycieczki');
+  // });
+  //
+  // it('Should display available trips to the logged-in user', function() {
+  //   loginScreen.loginProcess();
+  //   tripsScreen.get();
+  //   tripsScreen.getTrips().its('length').should('be.gt', 0);
+  // });
+  //
+  // it('Should allow user to view trip details', function() {
+  //   loginScreen.loginProcess();
+  //   tripsScreen.route();
+  //   tripsScreen.getTripsMainSections().first().click();
+  //   cy.location('pathname').should('contain', '/wycieczka');
+  // });
+  //
   it('Should allow user to add a trip to the cart', function() {
-    loginScreen.loginProcess();
-    tripsScreen.get();
-    tripsScreen.getTripsMainSections().first().click();
-    tripsDetails.getAddTCartButton().click();
+    addFirstTripToCart();
     cart.getOpenCartToggle().click();
     cart.getRows().its('length').should('be.gt', 0);
   });
+
+  it('Should allow user to make a reservation after filling the cart', function() {
+    addFirstTripToCart();
+    cart.getGoToCartButton().click();
+    cart.getQuantityInput().type(1);
+    cart.getCheckoutButton().click();
+    cart.getCheckoutConfirmButton().click();
+    cy.location('pathname').should('contain', afterOrder.getEndpoint());
+    afterOrder.getMessage().should('exist');
+    // thank-you-for-order-msg
+  });
+
 });
